6d1f13075c3f66e15df1174ce9a5e669a2867082
Vansh Ahuja <139003501+Vanshahuja1@users.noreply.github.com>
2025-03-05 19:40:42 +0530

    Update axiosInstance.ts

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL || "https://finance-tracker-back.onrender.com";
 
 const instance = axios.create({
     baseURL: API_URL + "/api",
